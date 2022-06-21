0faf7d654e2bb99b559fe0b608d5016d7e4df0fd
Fagundes <fagundesraphael7@gmail.com>
2022-06-21 13:11:19 -0400

    "Primeiro commit de aulas Java"

---
+++ Java/PrimeiroProj/src/classes/Drink.java
@@ -0,0 +1,21 @@
+package classes;
+import java.util.Scanner;
+
+public class Drink {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Fala a idade e passa o RG:");
+
+        int age = scanner.nextInt();
+
+        String msg = age < 18 ? "Não pode ir na baladinha" : "Pode ir na baladinha";
+
+        System.out.println(msg);
+
+        scanner.close();
+    }
+    
+}
---
+++ Java/PrimeiroProj/src/classes/GetItem2.java
@@ -0,0 +1,25 @@
+package classes;
+import java.util.Scanner;
+import java.util.Arrays;
+public class GetItem2 {
+    
+
+    public static void main(String[] args) {
+
+        String names[] = {"Paulo", "Jonas", "Caio"};
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe seu nome:");
+        String name = scanner.nextLine();
+
+
+        if (Arrays.asList(names).contains(name)) {
+            System.out.println("Nome encontrando, você é um convidado!");
+        } else {
+            System.out.println("Seu nome não está na lista, vaza penetra!");
+        }
+        scanner.close();
+
+    }
+}
